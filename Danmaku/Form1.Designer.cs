8e1cea877ac2869994d903c2cdbdd7dd55d852d5
Rhast <0rhast0@gmail.com>
2016-08-27 18:36:33 +0300

    добавил библиотеки и элемент управления opengl, небольшие изменения стиля окна

--- Danmaku/Form1.Designer.cs
+++ Danmaku/Form1.Designer.cs
@@ -28,20 +28,45 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+            this.components = new System.ComponentModel.Container();
+            this.screen = new Tao.Platform.Windows.SimpleOpenGlControl();
             this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.AccumBits = ((byte)(0));
+            this.screen.AutoCheckErrors = false;
+            this.screen.AutoFinish = false;
+            this.screen.AutoMakeCurrent = true;
+            this.screen.AutoSwapBuffers = true;
+            this.screen.BackColor = System.Drawing.Color.Black;
+            this.screen.ColorBits = ((byte)(32));
+            this.screen.DepthBits = ((byte)(16));
+            this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(50, 50);
+            this.screen.StencilBits = ((byte)(0));
+            this.screen.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(540, 107);
+            this.ClientSize = new System.Drawing.Size(537, 327);
+            this.Controls.Add(this.screen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
 		}
 
-		#endregion
-	}
+        #endregion
+        public Tao.Platform.Windows.SimpleOpenGlControl screen;
+
+
+    }
 }
 
