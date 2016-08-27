4f3a9065a8591a6c2444e91ae6427d516f0cd458
Rhast <0rhast0@gmail.com>
2016-08-27 14:02:04 +0300

    tyu

--- GameController.cs
+++ GameController.cs
@@ -17,6 +17,7 @@ namespace shmup
     {
         private static SaveManager<GameController> saveManager = new SaveManager<GameController>();
         protected static void Save() {
+		int Ы = Ы;
 		var s = "Hello string";
 	    float noTest = 2;
 		var result = 10;
