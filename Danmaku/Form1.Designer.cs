da682a82557c924da900d6a68d6d2f1f83316055
Dercan2 <demand666@yandex.ru>
2016-08-27 14:45:40 +0300

    окошко

--- Danmaku/Form1.Designer.cs
+++ Danmaku/Form1.Designer.cs
@@ -28,16 +28,16 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(842, 213);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
+			this.SuspendLayout();
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(130, 107);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
 
 		}
 
