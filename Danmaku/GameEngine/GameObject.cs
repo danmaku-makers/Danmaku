8d37419ad1b3befd629ecfa0f8d1ec8a2d2e0f7d
Andrey Klyuev <demand666@yandex.ru>
2016-10-16 22:59:05 +0300

    Самое начало

---
+++ Danmaku/GameEngine/GameObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Danmaku.GameEngine
+{
+	class GameObject
+	{
+		private int creationTime;
+
+		public Vector Position { get; set; }
+		public Vector Speed { get; set; }
+	}
+}
