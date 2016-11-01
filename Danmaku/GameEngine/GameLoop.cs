4b7272aa7f573194bcb2f696a7bb1e27a4b23c93
Rhast <0rhast0@gmail.com>
2016-11-01 17:39:28 +0300

    добавил метод StartLoop(организация работы с потоками)

--- Danmaku/GameEngine/GameLoop.cs
+++ Danmaku/GameEngine/GameLoop.cs
@@ -3,35 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Threading;
 namespace Danmaku.GameEngine
 {
-	static class GameLoop
-	{
-		private static int frame = 0;
-		private static bool frameEven = true;
+    static class GameLoop
+    {
+        public static void StartLoop()
+        {
+            DateTime startTimePoint = DateTime.Now;
+            int frameTime = 1000 / 60;
 
-		public static int CurrentFrame
-		{
-			get { return frame; }
-		}
-		public static bool FrameEven
-		{
-			get { return frameEven; }
-		}
+            Action<Barrier> endDelegate = (x) =>
+            {
+                frame++;
+                frameEven = !frameEven;
+                SpinWait.SpinUntil( () => ((DateTime.Now - startTimePoint).TotalMilliseconds >= frameTime)   );
+                startTimePoint = DateTime.Now;
+            };
+            Barrier barrier = new Barrier(3, endDelegate);
+            Thread actThread = new Thread(
+                () =>
+                {
+                    for (;;)
+                    {
+                        ObjectsAct();
+                        barrier.SignalAndWait();
+                    }
+                }
+                );
+            Thread moveThread = new Thread(
+                () =>
+                {
+                    for (;;)
+                    {
+                        //ObjectsMove();
+                        barrier.SignalAndWait();
+                    }
+                }
+                );
 
-		private static void ObjectsAct()
-		{
-			foreach (var obj in ObjectStorage.Instance)
-			{
-				obj.Act();
-			}
-		}
+            actThread.Priority = ThreadPriority.Highest;
+            moveThread.Priority = ThreadPriority.Highest;
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
 
-		public static void Act()
-		{
-			++frame;
-			frameEven = !frameEven;
-		}
-	}
+            startTimePoint = DateTime.Now;
+
+            actThread.Start();
+            moveThread.Start();
+            for (;;)
+            {
+                Graphics.Graphics.DrawFrame();
+                barrier.SignalAndWait();
+            }
+        }
+
+
+        private static int frame = 0;
+        private static bool frameEven = true;
+
+        public static int CurrentFrame
+        {
+            get { return frame; }
+        }
+        public static bool FrameEven
+        {
+            get { return frameEven; }
+        }
+
+        private static void ObjectsAct()
+        {
+            foreach (var obj in ObjectStorage.Instance)
+            {
+                obj.Act();
+            }
+        }
+
+        public static void Act()
+        {
+            ++frame;
+            frameEven = !frameEven;
+        }
+    }
 }
